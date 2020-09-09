63dd2200f9af943bcd76be89a37981d878552f28
eshux <54838488+eshux@users.noreply.github.com>
2020-09-09 13:18:15 +0300

    00 easy

--- exercises/01-easy/src/00-acronym/index.ts
+++ exercises/01-easy/src/00-acronym/index.ts
@@ -6,6 +6,20 @@
  * Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
  */
 
-function parse(input: string) {}
+function parse(input: string) {
+    let newString = input.replace(/[_-]/g,' ');
+
+    const acron = [];
+    const arr = newString.split(' ');
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].charAt(0).match(/[A-Z]/i)) {
+        acron.push(arr[i].charAt(0));
+    }
+    }
+
+return acron.join('').toUpperCase();
+
+}
 
 export { parse };
