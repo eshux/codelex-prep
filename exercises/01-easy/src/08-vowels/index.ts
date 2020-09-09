8e1329311616be30d3cbcbdcb35c986d3725faef
eshux <54838488+eshux@users.noreply.github.com>
2020-09-09 14:36:47 +0300

    08easy

--- exercises/01-easy/src/08-vowels/index.ts
+++ exercises/01-easy/src/08-vowels/index.ts
@@ -9,6 +9,20 @@
  * vowels('Hello there!') === 4
  */
 
-function vowels(s: string) {}
+function vowels(s: string) {
+    const lowS = s.toLowerCase();
+    const arr = lowS.split('');
+    const vowArr = ['a', 'e', 'o', 'i', 'u'];
+    const allVow = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < vowArr.length; j++) {
+            if (arr[i] === vowArr[j]) {
+                allVow.push(arr[i]);
+            }
+        }
+    }
+    return allVow.length;
+    
+}
 
 export { vowels };
