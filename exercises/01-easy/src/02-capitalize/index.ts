77f8ad8816c5db7374a68c0475a9ff4efd155fdc
eshux <54838488+eshux@users.noreply.github.com>
2020-09-09 13:40:38 +0300

    02 easy

--- exercises/01-easy/src/02-capitalize/index.ts
+++ exercises/01-easy/src/02-capitalize/index.ts
@@ -11,6 +11,13 @@
  * capitalize('look, it is working!') === 'Look, It Is Working!'
  */
 
-function capitalize(str: string) {}
+function capitalize(str: string) {
+    const arr = str.split(' ');
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    return arr.join(' ');
+}
 
 export { capitalize };
