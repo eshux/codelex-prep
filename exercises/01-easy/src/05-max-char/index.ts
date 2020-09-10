8148f5eee41f3442bbbc411a6b6354a171dcbfca
eshux <54838488+eshux@users.noreply.github.com>
2020-09-10 10:24:03 +0300

    05 unfinished

--- exercises/01-easy/src/05-max-char/index.ts
+++ exercises/01-easy/src/05-max-char/index.ts
@@ -9,6 +9,23 @@
  * maxChar("apple 1231111") === "1"
  */
 
-function maxChar(str: string) {}
+function maxChar(str: string) {
+    const strArr = str.split("").sort();
+    const arr = [];
+  
+    // let i = 0;
+    // while (i < strArr.length) {
+    //   if (strArr.charAt(0) === strArr.charAt(0+1)) {
+    //     arr.push(strArr.charAt(0));
+    //   }
+    //   i++;
+    // }
+  
+    for (let i = 0; i < strArr.length; i++) {
+      if (strArr[i] === strArr[i + 1] || strArr[i] === strArr[i - 1]) {
+        arr.push(strArr[i]);
+      }
+    }
+}
 
 export { maxChar };
