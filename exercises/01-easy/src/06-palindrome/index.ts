62cc39b46affc494abc9fe1a4033912275ed20ba
eshux <54838488+eshux@users.noreply.github.com>
2020-09-10 10:52:39 +0300

    06

--- exercises/01-easy/src/06-palindrome/index.ts
+++ exercises/01-easy/src/06-palindrome/index.ts
@@ -13,6 +13,24 @@
  * palindrome("abcdefg") === false
  */
 
-function palindrome(str: string) {}
+function palindrome(str: string) {
+    const strArr = str.split('');
+    const arr1 = [];
+    const arr2 = [];
+  
+    for (let i = 0; i < Math.ceil(strArr.length/2); i++) {
+        arr1.push(strArr[i]);
+    }
+    for (let j = strArr.length-1; j > Math.floor(strArr.length/2)-1; j--) {
+        arr2.push(strArr[j]);
+    }
+  
+  
+    if (arr1.join('') === arr2.join('')) {
+      return true;
+    } else {
+      return false;
+    }
+}
 
 export { palindrome };
