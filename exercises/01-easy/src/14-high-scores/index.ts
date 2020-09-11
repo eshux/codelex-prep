0fd6640a756adac22ebe8fdecd70e444f2a9da17
eshux <54838488+eshux@users.noreply.github.com>
2020-09-11 11:17:07 +0300

    14

--- exercises/01-easy/src/14-high-scores/index.ts
+++ exercises/01-easy/src/14-high-scores/index.ts
@@ -11,16 +11,48 @@ class HighScores {
   }
 
   get latest() {
-    return 0;
+    return this.scores[this.scores.length-1];
   }
 
   get personalBest() {
-    return 0;
+    return this.highScoreFunc(this.scores);
   }
 
   get personalTopThree() {
-    return 0;
+
+    const high = this.highScoreFunc (this.scores);
+    const arr1 = [...this.scores];
+    arr1.splice(arr1.indexOf(high), 1);
+  
+    const high2 = this.highScoreFunc(arr1);
+    arr1.splice(arr1.indexOf(high2), 1);
+
+    const high3 = this.highScoreFunc(arr1);
+    
+    const result: number[] = [high, high2, high3];
+    
+    if(this.scores.length >= 3) {
+      return result;
+    } else if (this.scores.length >= 2) {
+      result.pop();  
+      return result;
+    } else {
+      result.pop();
+      result.pop();
+      return result;
   }
+  } 
+
+  highScoreFunc (arr: number[]) {
+    let highscore = arr[0];
+    arr.forEach(i => {
+      if (i > highscore) {
+        highscore = i;
+      } 
+    })
+    return highscore;
+  }
+
 }
 
 export { HighScores };
